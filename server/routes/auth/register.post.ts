c22fa954be83e21b11fd2ac981eefa33fdd2607e
2406584 <2406584@abertay.ac.uk>
2025-10-27 16:34:06 +0000

    feat: Included hashing for registration

--- server/routes/auth/register.post.ts
+++ server/routes/auth/register.post.ts
@@ -1,12 +1,27 @@
-import { defineEventHandler, readBody } from "h3"
+import { createError, defineEventHandler, readBody } from "h3"
+import argon2 from 'argon2'
+
 
 export default defineEventHandler(async event => {
   const body =  await readBody(event);
 
-  const { username, password } = body;
+  const {username , password} = body
+  
 
-  return {
-    "username":username,
-    "password":password
+  if (!username || !password) {
+    throw createError({ statusCode: 400, message: 'Missing username or password' })
+  }
+
+  try {
+    // Hash the password using Argon2id (recommended)
+    const hash = await argon2.hash(password, {
+      type: argon2.argon2id
+    })
+
+    // TODO: Save `username` and `hash` to your database
+    return { message: 'User registered successfully' }
+  } catch (err) {
+    console.error(err)
+    throw createError({ statusCode: 500, message: 'Error hashing password' })
   }
 })
