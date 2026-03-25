df275e273d322c1be26637802f9491e4db11e57e
2406584 <2406584@abertay.ac.uk>
2026-03-25 18:38:23 +0000

    chore: update Prisma and PostgreSQL dependencies, remove unused migrations, and refactor database connection
    - Updated Prisma and @prisma/client to version 7.5.0 in package.json.
    - Removed old migration files and migration lock related to users and login metrics.
    - Refactored prisma.config.ts to use process.env for DATABASE_URL.
    - Updated schema.prisma to reflect changes in the Users and UsersLogin models.
    - Modified GetDB utility to use PrismaPg adapter for PostgreSQL connection.

--- server/utils/GetDB.ts
+++ server/utils/GetDB.ts
@@ -1,4 +1,9 @@
-import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
 export function GetDB() {
-     return new PrismaClient()
+     return new PrismaClient({ adapter })
 }
