8a0e5f3500b431671c8d514f0706fc0be2cf9299
2406584 <2406584@abertay.ac.uk>
2026-04-15 19:48:04 +0100

    feat: add seed script for initializing superadmin user in the database

---
+++ prisma/seed.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import argon2 from 'argon2'
+import 'dotenv/config'
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+})
+const prisma = new PrismaClient({ adapter })
+
+async function main() {
+  const username = 'admin'
+  const password = 'superpassword' // Remember to change this in a production environment
+
+  const existingAdmin = await prisma.users.findFirst({
+    where: { username, role: 'SUPERADMIN' }
+  })
+
+  if (!existingAdmin) {
+    const hash = await argon2.hash(password, { type: argon2.argon2id })
+    const superAdmin = await prisma.users.create({
+      data: {
+        username,
+        password: hash,
+        role: 'SUPERADMIN',
+        neuro: false,
+      }
+    })
+    console.log('Superadmin created:', superAdmin.username)
+  } else {
+    console.log('Superadmin already exists')
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
